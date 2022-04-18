664bb3af0e4f5c5865f74668439af176d86d1844
Ashiqur Rahman Prince <rashiqur92@gmail.com>
2022-04-18 20:35:08 +0600

    1. add view table from database user_registration 2. delete_user account from database 3. dual connection (Oracle and MySQL)

--- admin/delete_user.php
+++ admin/delete_user.php
@@ -1,24 +1,23 @@
-<?php 
+<?php
+include "../connection.php";
 // ------------------------------------------------------------- MySqli Connection Setup Start------------------------------------------------------------
 
-$link = mysqli_connect("localhost", "root", "") or die(mysqli_error($link));
-mysqli_select_db($link, "php_ims") or die(mysqli_error($link)); //php_ims is the database name
+$id = $_GET["USERNAME"];
+mysqli_query($link, "DELETE FROM user_registration WHERE USERNAME='$id'");
 
 // ------------------------------------------------------------- MySqli Connection Setup END--------------------------------------------------------------
 
 // ------------------------------------------------------------ Oracle Connection Setup Start-------------------------------------------------------------
 
-// $db = "(DESCRIPTION=(ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = localhost)(PORT = 1521)))(CONNECT_DATA=(SID=orcl)))" ;
-//             if($conn = OCILogon("prince", "1234", $db))
-//             {
-//                 // echo "Successfully connected to Oracle.";
-//             }
-//             else
-//             {
-//                 $err = OCIError();
-//                 echo "Connection failed." . $err['text'];
-//             }
+// $id = $_GET["USERNAME"];
+// $query = "DELETE FROM user_registration WHERE USERNAME='$id'";
+// $result = oci_parse($conn, $query);
+// oci_execute($result);
 
 // ------------------------------------------------------------ Oracle Connection Setup End---------------------------------------------------------------
 
 ?>
+
+<script type="text/javascript">
+    window.location = "add_new_user.php";
+</script>
